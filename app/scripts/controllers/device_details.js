4d5f4ffe7d32101d203b23a065786731f33d9203
Federico De Faveri <federico.defaveri@fao.org>
2015-07-24 13:26:42 +0200

    Added attribute editing

--- app/scripts/controllers/device_details.js
+++ app/scripts/controllers/device_details.js
@@ -4,5 +4,46 @@ angular.module("devicesUiApp")
 .controller("DeviceDetailsCtrl", ["$route", "$log", "selectionservice", "searchservice", "device", 
            function ($route, $log, selectionservice, searchservice, device) {
 	
-	  this.device = device;
+	var self = this;
+	this.device = device;
+	
+	this.attribute = null;
+	this.nameField = "";
+	this.valueField = "";
+	  
+	this.editAttribute = function(attribute) {
+		self.attribute = attribute;
+		self.nameField = attribute.name;
+		self.valueField = attribute.value;		
+		$('#editAttributeModal').modal();
+	};
+	  
+	this.editNewAttribute = function() {
+		self.attribute = null;
+		self.nameField = "";
+		self.valueField = "";
+		$('#editAttributeModal').modal();
+	};
+	
+	this.saveAttribute = function() {
+		if (self.attribute!=null) {
+			self.attribute.name = self.nameField;
+			self.attribute.value = self.valueField;
+		} else {
+			self.device.attributes.push({name:self.nameField, value:self.valueField});
+		}
+		$('#editAttributeModal').modal('hide');
+	};
+	
+	this.removeAttribute = function(attribute) {
+		self.attribute = attribute;
+		$('#removeAttributeModal').modal();
+	};
+	
+	this.deleteAttribute = function() {
+		var index = device.attributes.indexOf(self.attribute);
+		$log.info("index: "+index);
+		if (index>=0) device.attributes.splice(index, 1);
+		$('#removeAttributeModal').modal('hide');
+	};
 }]);
